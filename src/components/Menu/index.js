9e6a7f9b399e56908ed7082f40187adaee73f3d0
Marcus Vinícius <showdeart.mv@gmail.com>
2020-07-28 20:37:47 -0300

    Commit Pages

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import Logo from '../../assets/images/Logo.png';
 import './Menu.css';
-import Button from "../Button"
+import Button from "../Button";
 
 function Menu(){
     return(
         <nav className = "Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt='ViniFlix logo'/>
-            </a>
-            <Button as="a" className = "ButtonLink" href="/">
+            </Link>
+            <Button as={Link} className = "ButtonLink" to="/cadastro/video">
                Novo Vídeo
             </Button>
         </nav>

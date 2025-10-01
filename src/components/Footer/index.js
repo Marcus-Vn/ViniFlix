5f1bd34c4bb6b2d70f4e5cd40baf1e40dfc1254d
Marcus Vinícius <58491006+Marcus-Vn@users.noreply.github.com>
2025-10-01 12:13:21 -0300

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import Logo from '../../assets/images/Logo.png';
-import { FooterBase } from './styles';
+import React from "react";
+import Logo from "../../assets/images/Logo.png";
+import { FooterBase } from "./styles";
 
 function Footer() {
   return (
     <FooterBase>
       <a href="/">
-        <img className="Logo" src={Logo} alt='ViniFlix logo'/>
+        <img className="Logo" src={Logo} alt="ViniFlix logo" />
       </a>
       <p>
-        Orgulhosamente criado durante a
-        {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
-        </a>
+        Orgulhosamente criado durante a{" "}
+        <a href="https://www.alura.com.br/">Imersão React da Alura</a>
       </p>
     </FooterBase>
   );

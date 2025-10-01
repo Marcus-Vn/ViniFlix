267194972fee72f2404810b270340cee006d0e35
Marcus Vinícius <58491006+Marcus-Vn@users.noreply.github.com>
2025-10-01 12:05:52 -0300

    Update index.js

--- src/components/BannerMain/index.js
+++ src/components/BannerMain/index.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import VideoIframeResponsive from './components/VideoIframeResponsive';
-import { BannerMainContainer, ContentAreaContainer, WatchButton } from './styles';
+import React from "react";
+import PropTypes from "prop-types";
+import VideoIframeResponsive from "./components/VideoIframeResponsive";
+import { BannerMainContainer, ContentAreaContainer, WatchButton } from "./styles";
 
+// Função para extrair ID do YouTube
 function getYouTubeId(youtubeURL) {
   return youtubeURL
     .replace(
       /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/,
-      '$7',
+      "$7"
     );
 }
 
-export default function BannerMain({
-  videoTitle,
-  videoDescription,
-  url,
-}) {
+export default function BannerMain({ videoTitle, videoDescription, url }) {
   const youTubeID = getYouTubeId(url);
   const bgUrl = `https://img.youtube.com/vi/${youTubeID}/maxresdefault.jpg`;
 
@@ -22,24 +20,22 @@ export default function BannerMain({
     <BannerMainContainer backgroundImage={bgUrl}>
       <ContentAreaContainer>
         <ContentAreaContainer.Item>
-          <ContentAreaContainer.Title>
-            {videoTitle}
-          </ContentAreaContainer.Title>
-
-          <ContentAreaContainer.Description>
-            {videoDescription}
-          </ContentAreaContainer.Description>
+          <ContentAreaContainer.Title>{videoTitle}</ContentAreaContainer.Title>
+          <ContentAreaContainer.Description>{videoDescription}</ContentAreaContainer.Description>
         </ContentAreaContainer.Item>
 
         <ContentAreaContainer.Item>
-          <VideoIframeResponsive
-            youtubeID={youTubeID}
-          />
-          <WatchButton>
-            Assistir
-          </WatchButton>
+          <VideoIframeResponsive youtubeID={youTubeID} />
+          <WatchButton>Assistir</WatchButton>
         </ContentAreaContainer.Item>
       </ContentAreaContainer>
     </BannerMainContainer>
   );
 }
+
+// Declarando tipos das props
+BannerMain.propTypes = {
+  videoTitle: PropTypes.string.isRequired,
+  videoDescription: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+};

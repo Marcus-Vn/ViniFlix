8dbd4c016eb71f9032d0a5f75d99a39bba5a764b
Marcus Vinícius <58491006+Marcus-Vn@users.noreply.github.com>
2025-10-01 12:15:43 -0300

    Update index.js

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import Logo from '../../assets/images/Logo.png';
-import './Menu.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/images/Logo.png";
+import "./Menu.css";
 import Button from "../Button";
 
-function Menu(){
-    return(
-        <nav className = "Menu">
-            <Link to="/">
-                <img className="Logo" src={Logo} alt='ViniFlix logo'/>
-            </Link>
-            <Button as={Link} className = "ButtonLink" to="/cadastro/video">
-               Novo Vídeo
-            </Button>
-        </nav>
-    );
+function Menu() {
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="ViniFlix logo" />
+      </Link>
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo Vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
